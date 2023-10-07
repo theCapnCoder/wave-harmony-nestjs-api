7429263b14fe9508a9e2b8ba06cf791eafc20435
PavelM <myagkiypavel@gmail.com>
2023-10-07 12:42:09 +0300

    chore: auth controller

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,19 +1,19 @@
 import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
-import { LocaAuthlGuard } from '../guards/local.auth.guard';
+import { LocalAuthGuard } from '../guards/local.auth.guard';
 import { BearerAuthGuard } from '../guards/bearer.auth.guard';
+import { User } from '../../users/users.service';
 
 @Controller('auth')
 export class AuthController {
   @UseGuards(BearerAuthGuard)
   @Get('me')
-  me(): any {
-    return 'me';
+  me(@Request() req): User {
+    return req.user;
   }
 
-  @UseGuards(LocaAuthlGuard)
+  @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(@Request() req) {
-    console.log('enter');
-    return req.body;
+  login(@Request() req): User {
+    return req.user;
   }
 }
