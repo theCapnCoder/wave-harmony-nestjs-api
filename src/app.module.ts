19f03241cf80f0b8689683374cd19cdb6b26355d
PavelM <myagkiypavel@gmail.com>
2023-10-09 14:26:20 +0300

    feat: app config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 import { ExampleModule } from './examples/example.module';
 import { ExamplesController } from './examples/controllers/app.controller';
 import { ExamplesService } from './examples/services/app.service';
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, ExampleModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UsersModule,
+    ExampleModule,
+  ],
   controllers: [ExamplesController],
   providers: [ExamplesService],
 })
