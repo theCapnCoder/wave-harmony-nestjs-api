1b5288bf7ddcafd6e814d206ba902054a3fa2302
PavelM <myagkiypavel@gmail.com>
2023-10-09 19:57:38 +0300

    chore: app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,24 @@
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ExampleModule } from './examples/example.module';
 import { ExamplesController } from './examples/controllers/app.controller';
 import { ExamplesService } from './examples/services/app.service';
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from './config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => config.get('database')(),
+    }),
     AuthModule,
     UsersModule,
     ExampleModule,
